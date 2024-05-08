b81bac7afd60e0fb73845f0e6bf98cd02b89b8c5
anwesh <anwesh.anthati@phenom.com>
2024-05-08 14:17:36 +0530

    new file added

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
-        Main obj=new Main();
-        obj.toUpperCase("Anwesh");
+        System.out.println(tc01CheckEmpty());
+        System.out.println(tc02CheckSpecialCharacter());
+        System.out.println(tc03CheckNumbers());
+        System.out.println(tc04CheckAlphabitsWithNumbersSpecialCharacters());
+        System.out.println(tc05CheckAllLowerAlphabets());
+        System.out.println(tc06CheckSingleCharacter());
+        System.out.println(tc07CheckNull());
     }
 
-    public static void toUpperCase(String str){
+    public static String toUpperCase(String str){
         String s = "";
+
         for(int i=0 ; i<str.length();i++){
             if(Character.isLowerCase(str.charAt(i))){
                 s += (char)(str.charAt(i)-32);
@@ -15,6 +20,49 @@ public class Main {
                 s += (char)str.charAt(i);
             }
         }
-        System.out.println(s);
+        return s;
+    }
+
+    public static boolean tc01CheckEmpty(){
+        String input =" ";
+        String expected=" ";
+        return toUpperCase(input).equals(expected);
+    }
+
+    public static boolean tc02CheckSpecialCharacter(){
+        String input="@#$%^";
+        String expected="@#$%^";
+        return toUpperCase(input).equals(expected);
+
+    }
+
+    public static boolean tc03CheckNumbers(){
+        String input="123456789";
+        String expected="123456789";
+        return toUpperCase(input).equals(expected);
+    }
+
+    public static boolean tc04CheckAlphabitsWithNumbersSpecialCharacters(){
+        String input="abc1234@";
+        String expected="ABC1234@";
+        return toUpperCase(input).equals(expected);
+    }
+
+    public static boolean tc05CheckAllLowerAlphabets(){
+        String input="abcd";
+        String expected="ABCD";
+        return toUpperCase(input).equals(expected);
+    }
+
+    public static boolean tc06CheckSingleCharacter(){
+        String input="c";
+        String expected="C";
+        return toUpperCase(input).equals(expected);
+    }
+
+    public static boolean tc07CheckNull(){
+        String input=null;
+        String expected="Null pointer Exception";
+        return toUpperCase(input).equals(expected);
     }
 }
